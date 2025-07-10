64ffef3453a0de41d4da3e6d407962837abfea95
khoitran2003 <anhkhoi246813579@gmail.com>
2025-07-10 10:23:02 +0700

    modify main.js

--- src/main.js
+++ src/main.js
@@ -4,11 +4,11 @@ import path from "path";
 import { engine } from "express-handlebars";
 import { fileURLToPath } from "url";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
 const app = express();
 const port = 3000;
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 app.use(express.static(path.join(__dirname, "public")));
 app.use(morgan("combined"));
