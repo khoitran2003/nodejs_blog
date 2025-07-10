a421b111b40d79a190300d06cc9122aae7e7ccff
khoitran2003 <anhkhoi246813579@gmail.com>
2025-07-10 23:02:29 +0700

    modify src/main.js

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import morgan from "morgan";
 import path from "path";
 import { engine } from "express-handlebars";
 import { fileURLToPath } from "url";
+import route from "./routes/index.js";
 
 const app = express();
 const port = 3000;
@@ -11,7 +12,16 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 app.use(express.static(path.join(__dirname, "public")));
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+app.use(express.json());
+
+// http logger
 app.use(morgan("combined"));
+
 app.engine(
   "hbs",
   engine({
@@ -21,17 +31,8 @@ app.engine(
 app.set("view engine", "hbs");
 app.set("views", path.join(__dirname, "resources/views"));
 
-// route
-app.get("/", (req, res) => {
-  res.render("home", {
-    title: "Home Page",
-    user: "Khoi",
-  });
-});
-
-app.get("/news", (req, res) => {
-  res.render("news");
-});
+// Route initialize
+route(app);
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
